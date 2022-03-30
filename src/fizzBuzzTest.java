746f6d29beeeee3cc5a15a31fce7cf6f681375b8
BINHNGHIEN <haibinh.94.bui@gmail.com>
2022-03-30 13:39:13 +0700

    noidung

---
+++ src/fizzBuzzTest.java
@@ -0,0 +1,6 @@
+public class fizzBuzzTest  extends  fizzBuzz{
+    public static void main(String[] args) {
+        fizzBuzz buzz= new fizzBuzzTest();
+        System.out.println(buzz.fizzBuzz(5));
+    }
+}
